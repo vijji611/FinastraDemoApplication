421b47c6a2239cf03072ed4e0f5695d0a8b65443
Hemesh <Hemesh@DESKTOP-HBUVCKG>
2020-09-16 15:29:32 +0530

    Added JUnit tests

--- FinastraDemoApplication/src/main/java/com/fin/demo/DemoClass.java
+++ FinastraDemoApplication/src/main/java/com/fin/demo/DemoClass.java
@@ -4,5 +4,17 @@ package com.fin.demo;
 public class DemoClass {
 	public static void main(String args[]) {
 		System.out.println("Test...");
+		String res = play(Integer.parseInt(args[0]));
+		System.out.println(res);
 	}
+	
+	public static String play(int number) {
+
+        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+        if (number % 3 == 0) return "Divisible by 3";
+        if (number % 5 == 0) return "Divisible by 5";
+
+
+        return String.valueOf(number);
+    }
 }
---
+++ FinastraDemoApplication/src/test/java/DemoClassTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.fin.demo.DemoClass;
+
+class DemoClassTest {
+
+	 public DemoClass demoClass;
+
+	    @BeforeEach
+	    public void setUp() {
+	        demoClass = new DemoClass();
+	    }
+
+	    @DisplayName("Play DemoClass with number = 1")
+	    @Test
+	    public void testNumber() {
+	        String res = demoClass.play(1);
+	        Assertions.assertEquals(res, "1");
+	    }
+
+	    @DisplayName("Play DemoClass with number = 3")
+	    @Test
+	    public void test3() {
+	        String res = demoClass.play(3);
+	        Assertions.assertEquals(res, "Divisible by 3");
+	    }
+
+	    @DisplayName("Play DemoClass with number = 5")
+	    @Test
+	    public void test5() {
+	        String res = demoClass.play(5);
+	        Assertions.assertEquals(res, "Divisible by 5");
+	    }
+
+	    @DisplayName("Don't Play DemoClass with number = 0")
+	    @Test
+	    public void testZero() {
+
+	        Assertions.assertThrows(IllegalArgumentException.class,
+	                () -> demoClass.play(0));
+	    }
+
+	    @AfterEach
+	    public void tearDown() {
+	        demoClass = null;
+	    }
+
+}
